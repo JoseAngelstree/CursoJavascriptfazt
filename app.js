23b0627f59d068cb134ed6ab1f2c8e29ee1fc9b0
Angel <joseangelestrada29@gmail.com>
2021-06-04 19:03:16 -0500

    Curso de Javascript

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
     document.write("<h1> Hola Mundo desde Javascript </h1>");
     console.error("Error 501");
+    console.log(  [1,2,3,4,5] );
+
+    var dato = "Hola mundo desde Javascript con la Consola";
+    let dia = 29;
+    dia = dia * 5;
+    const gato = 25;
+    gato  = gato * 25;
+    console.log(gato);
